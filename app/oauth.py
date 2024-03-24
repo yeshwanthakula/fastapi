e87e2a7fed8f929c2ff1e8c460506f76b5dd5d77
Yeshwanth Akula <yeshwanthakula@Yeshwanths-MacBook-Air.local>
2024-03-24 16:34:37 +0530

    updated the auth details

--- app/oauth.py
+++ app/oauth.py
@@ -4,11 +4,11 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from . import schemas
+from .config import settings
 
-
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
